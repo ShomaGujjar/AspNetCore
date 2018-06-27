c31299815e85a91124020f66b9058c025f92117e
Shoma Gujjar <shoma.gujjar@gmail.com>
2018-06-27 14:35:06 +0100

    Introducing javascript.

---
+++ AspNetCoreTutorials/AspNetCoreTutorials/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿(function() {
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Jai Sai Ram";
+
+
+    var main = document.getElementById("main");
+    main.onmouseenter = function() {
+        main.style.background = "#888";
+    };
+
+    main.onmouseleave = function() {
+        main.style.background = "";
+    };
+})();
+
